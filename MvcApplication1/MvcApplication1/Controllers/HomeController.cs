0ea8112f961b830c3b3efdf8c6cb897eb34b16d4
BrijPurohit <purohit.brijmohan@gmail.com>
2015-03-30 02:53:12 +0530

    start Try me
    try me initial code

---
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "This is about Try Application.";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Try App";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Try Contact Page..";
+
+            return View();
+        }
+    }
+}
